f76b519ae2aeec74735e4ea9026ad9e45f656945
i4M1k0SU <yuuta_nakano@dwango.co.jp>
2018-11-12 15:17:08 +0900

    Support IPv6 block.

--- blockAppleOCSPServer/main.m
+++ blockAppleOCSPServer/main.m
@@ -3,15 +3,31 @@
 
 void blockAppleOCSPServer(void) {
     NSString *hostsFile = [NSString stringWithContentsOfFile:@"/etc/hosts" encoding:NSUTF8StringEncoding error:nil];
-    if ([hostsFile rangeOfString:@"\n127.0.0.1    ocsp.apple.com\n"].location == NSNotFound && [hostsFile rangeOfString:@"\n127.0.0.1 ocsp.apple.com\n"].location == NSNotFound){
-        FILE *file = fopen("/etc/hosts","a");
-        fprintf(file, "127.0.0.1    ocsp.apple.com\n");
-        fclose(file);
-        
-        run("killall -9 mDNSResponder");
-        
-        NSLog(@"Apple OCSP server blocked successfully");
+
+    NSRegularExpression *ipv4RegExp = [NSRegularExpression regularExpressionWithPattern:@"^127.0.0.1[\\s|\t]+ocsp.apple.com$" options:0 error:nil];
+    NSRegularExpression *ipv6RegExp = [NSRegularExpression regularExpressionWithPattern:@"^::1[\\s|\t]+ocsp.apple.com$" options:0 error:nil];
+
+    NSTextCheckingResult *ipv4IsMatched = [ipv4RegExp firstMatchInString:hostsFile options:0 range:NSMakeRange(0, hostsFile.length)];
+    NSTextCheckingResult *ipv6IsMatched = [ipv6RegExp firstMatchInString:hostsFile options:0 range:NSMakeRange(0, hostsFile.length)];
+
+    // already blocked
+    if (ipv4IsMatched && ipv6IsMatched) {
+        return;
     }
+
+    FILE *file = fopen("/etc/hosts","a");
+
+    if (!ipv4IsMatched) {
+        fprintf(file, "127.0.0.1    ocsp.apple.com\n");
+    }
+
+    if (!ipv6IsMatched){
+        fprintf(file, "::1    ocsp.apple.com\n");
+    }
+
+    fclose(file);
+    run("killall -9 mDNSResponder");
+    NSLog(@"Apple OCSP server blocked successfully");
 }
 
 int main(int argc, char **argv, char **envp) {
--- unblockAppleOCSPServer/main.m
+++ unblockAppleOCSPServer/main.m
@@ -2,16 +2,38 @@
 #import "../utils.h"
 
 void unblockAppleOCSPServer(void) {
-    NSString *hostsFile = [NSString stringWithContentsOfFile:@"/etc/hosts" encoding:NSUTF8StringEncoding error:nil];
-    if ([hostsFile rangeOfString:@"\n127.0.0.1    ocsp.apple.com\n"].location != NSNotFound){
-        FILE *file = fopen("/etc/hosts","w");
-        fprintf(file, "%s", [[hostsFile stringByReplacingCharactersInRange:[hostsFile rangeOfString:@"127.0.0.1    ocsp.apple.com\n"] withString:@""] UTF8String]);
-        fclose(file);
-        
-        run("killall -9 mDNSResponder");
-        
-        NSLog(@"Apple OCSP server unblocked successfully");
+    NSMutableString *hostsFile = [NSMutableString stringWithContentsOfFile:@"/etc/hosts" encoding:NSUTF8StringEncoding error:nil];
+
+    NSRegularExpression *ipv4RegExp = [NSRegularExpression regularExpressionWithPattern:@"^127.0.0.1[\\s|\t]+ocsp.apple.com$" options:0 error:nil];
+    NSRegularExpression *ipv6RegExp = [NSRegularExpression regularExpressionWithPattern:@"^::1[\\s|\t]+ocsp.apple.com$" options:0 error:nil];
+
+    NSTextCheckingResult *ipv4IsMatched = [ipv4RegExp firstMatchInString:hostsFile options:0 range:NSMakeRange(0, hostsFile.length)];
+    NSTextCheckingResult *ipv6IsMatched = [ipv6RegExp firstMatchInString:hostsFile options:0 range:NSMakeRange(0, hostsFile.length)];
+
+    // non blocked
+    if (!ipv4IsMatched && !ipv6IsMatched) {
+        return;
     }
+
+    NSMutableArray *matches = [NSMutableArray array];
+
+    if (ipv4IsMatched) {
+        [matches addObjectsFromArray: [ipv4RegExp matchesInString:hostsFile options:0 range:NSMakeRange(0, hostsFile.length)]];
+    }
+
+    if (ipv6IsMatched){
+        [matches addObjectsFromArray: [ipv6RegExp matchesInString:hostsFile options:0 range:NSMakeRange(0, hostsFile.length)]];
+    }
+
+    [matches enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(NSTextCheckingResult* match, NSUInteger idx, BOOL *stop) {
+            [hostsFile deleteCharactersInRange:match.range];
+    }];
+
+    FILE *file = fopen("/etc/hosts","w");
+    fprintf(file, "%s", [hostsFile UTF8String]);
+    fclose(file);
+    run("killall -9 mDNSResponder");
+    NSLog(@"Apple OCSP server unblocked successfully");
 }
 
 int main(int argc, char **argv, char **envp) {
